419c5a8b46fd5e04b99d8bacfac85ee9fd29489d
Sarah Agemo <agemosarah01@gmail.com>
2024-09-19 16:07:06 +0300

    Linked pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,58 @@
-import Home from './pages/home/Home'
-import TopBar from './components/topbar/TopBar'
-import Single from './pages/single/Single'
-import Write from './pages/write/Write'
-import Settings from './pages/settings/Settings'
+// import React from 'react';
+// import Home from './pages/home/Home'
+// import TopBar from './components/topbar/TopBar'
+// import Single from './pages/single/Single'
+// import Write from './pages/write/Write'
+// import Settings from './pages/settings/Settings'
+// import Login from './pages/login/Login'
+// import Register from './pages/register/Register'
 
+
+
+// function App() {
+//   return (
+   
+//     <>
+//       <TopBar />
+//       <Home />
+//     </>
+    
+ 
+
+    
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopBar from './components/topbar/TopBar';
+import Home from './pages/home/Home';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Post from './components/post/Post';
 
 function App() {
+  const user = true;
   return (
-    <>
-    
-     <TopBar />
-     <Settings />
-
-    </>
-  )
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Register /> :<Register />} />
+        <Route path="/login" element={user ? <Login /> :<Login />} />  
+        <Route path="/write" element={user ? <Write /> :<Write />} />
+        <Route path="/settings" element={user ? <Settings /> :<Register />} />
+        <Route path="/post/:postId" element={<Single /> } />
+                  
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,41 @@
-import './TopBar.css'
+// import './TopBar.css'
+
+// function TopBar() {
+//   return (
+//     <div className='top'>
+//       <div className='topLeft'>
+//         <i className="topIcon fa-brands fa-facebook"></i>
+//         <i className="topIcon fa-brands fa-square-x-twitter"></i>
+//         <i className="topIcon fa-brands fa-linkedin"></i>
+//         <i className="topIcon fa-brands fa-square-instagram"></i>
+//       </div>
+//       <div className='topCenter'>
+//         <ul className='topList'>
+//             <li className='topListItem'>HOME</li>
+//             <li className='topListItem'>ABOUT</li>
+//             <li className='topListItem'>CONTACT</li>
+//             <li className='topListItem'>WRITE</li>
+//             <li className='topListItem'>LOGOUT</li>
+//         </ul>
+//       </div>
+//       <div className='topRight'>
+//         <img 
+//         className='topImg'
+//         src="https://avatars.githubusercontent.com/u/160914323?v=4" alt="" />
+//         <i className=" topSearchIcon fas fa-search"></i>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default TopBar
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './TopBar.css';
 
 function TopBar() {
+  const user = false;
   return (
     <div className='top'>
       <div className='topLeft'>
@@ -11,21 +46,54 @@ function TopBar() {
       </div>
       <div className='topCenter'>
         <ul className='topList'>
-            <li className='topListItem'>HOME</li>
-            <li className='topListItem'>ABOUT</li>
-            <li className='topListItem'>CONTACT</li>
-            <li className='topListItem'>WRITE</li>
-            <li className='topListItem'>LOGOUT</li>
+          <li className='topListItem'>
+            <Link to="/" className="link">HOME</Link>
+          </li>
+          <li className='topListItem'>
+            <Link to="/about" className="link">ABOUT</Link>
+          </li>
+          <li className='topListItem'>
+            <Link to="/contact" className="link">CONTACT</Link>
+          </li>
+          <li className='topListItem'>
+            <Link to="/write" className="link">WRITE</Link>
+          </li>
+          <li className='topListItem'>
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className='topRight'>
-        <img 
-        className='topImg'
-        src="https://avatars.githubusercontent.com/u/160914323?v=4" alt="" />
-        <i className=" topSearchIcon fas fa-search"></i>
+        {
+          user ? (
+
+            <img 
+              className='topImg'
+              src="https://avatars.githubusercontent.com/u/160914323?v=4" 
+              alt="Profile" 
+            />
+          ) : (
+            <ul className='topList'>
+              <li className='topListItem'>
+                <Link to="/login" className="link">
+                LOGIN
+                </Link>
+              </li>
+              <li className='topListItem'>
+                <Link to="/register" className="link">
+                REGISTER
+                </Link>
+              </li>
+             
+             
+            </ul> 
+          )
+        }
+        
+        <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
-  )
+  );
 }
 
-export default TopBar
+export default TopBar;
